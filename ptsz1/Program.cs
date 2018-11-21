74ae04eef9ec8d95665b3a7835683126672ef42c
etermaw <etermaw@gmail.com>
2018-11-21 23:13:56 +0100

    small fixes

--- ptsz1/Program.cs
+++ ptsz1/Program.cs
@@ -35,9 +35,11 @@ namespace ptsz1
 
             ProblemInstance instance = instances[k];
 
-            var result = solve(instance, h);
+            var result = solve2(instance, h);
             Console.WriteLine(instance.CalculateResult(h, result));
             Console.WriteLine(string.Join(" ", result.Select(x => string.Format("t{0}", x))));
+
+            int b = 0;
         }
 
         static List<ProblemInstance> GetInstances(List<int[]> rawData)
@@ -77,8 +79,37 @@ namespace ptsz1
         static IEnumerable<int> solve(ProblemInstance instance, float h)
         {
             return Enumerable.Range(0, instance.tasks.Count)
-                             .OrderBy(x => Math.Abs(instance.tasks[x][1] - instance.tasks[x][2]))
+                             .OrderBy(x => instance.tasks[x][2] - instance.tasks[x][1])
                              .ToList();
+        }
+
+        static IEnumerable<int> solve2(ProblemInstance instance, float h)
+        {
+            int deadline = instance.CalculateDeadline(h);
+            int current_time = 0;
+            int i = 0;
+
+            List<int> ret = new List<int>(instance.tasks.Count);
+
+            List<int> sorted_by_a = Enumerable.Range(0, instance.tasks.Count)
+                                              .OrderBy(x => instance.tasks[x][1] / instance.tasks[x][2])
+                                              .OrderBy(x => instance.tasks[x][1])
+                                              .ToList();
+
+            while(current_time < deadline && i < sorted_by_a.Count)
+            {
+                ret.Add(sorted_by_a[i]);
+                current_time += instance.tasks[i][0];
+                ++i;
+            }
+
+            IEnumerable<int> sorted_by_b = sorted_by_a.Skip(i)
+                                                      .OrderByDescending(x => instance.tasks[x][2]);
+
+            foreach(int task_num in sorted_by_b)
+                ret.Add(task_num);
+
+            return ret;
         }
     }
 }
